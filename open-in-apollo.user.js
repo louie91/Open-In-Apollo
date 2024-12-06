5af065369d0eb61d37657b4724e02255532c3f69
louie91 <126337672+louie91@users.noreply.github.com>
2024-12-06 12:54:26 +0100

    Update open-in-apollo.user.js

--- open-in-apollo.user.js
+++ open-in-apollo.user.js
@@ -3,9 +3,6 @@
 // @version      1.0.1
 // @author       AnthonyGress
 // @match        *://*.reddit.com/*
-// @downloadURL  https://gist.github.com/AnthonyGress/c04327abc6ff3bc45871468743f00341/raw/open-in-apollo.user.js
-// @updateURL    https://gist.github.com/AnthonyGress/c04327abc6ff3bc45871468743f00341/raw/open-in-apollo.user.js
-// @homepage     https://gist.github.com/AnthonyGress/c04327abc6ff3bc45871468743f00341/
 // ==/UserScript==
 if (window.location.pathname === '/redirect') return;
 window.location.href = `apollo2://${window.location.hostname}${window.location.pathname}`
