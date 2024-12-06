c54c7d7d10a9339ad7c0ab855925b9ae02e2fa01
louie91 <126337672+louie91@users.noreply.github.com>
2024-12-06 13:55:55 +0100

    Update open-in-apollo.user.js

--- open-in-apollo.user.js
+++ open-in-apollo.user.js
@@ -1,8 +1,11 @@
 // ==UserScript==
 // @name         Open Apollo App
 // @version      1.0.2
-// @author       AnthonyGress
+// @author       Original by AnthonyGress, modified by louie91
 // @match        *://*.reddit.com/*
+// @downloadURL  https://gist.github.com/louie91/1975a73500be2c8e0f9d94445af6fbd6/raw/open-in-apollo.user.js
+// @updateURL    https://gist.github.com/louie91/1975a73500be2c8e0f9d94445af6fbd6/raw/open-in-apollo.user.js
+// @homepage     https://gist.github.com/louie91/1975a73500be2c8e0f9d94445af6fbd6/
 // @inject-into content
 // ==/UserScript==
 
